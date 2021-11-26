6fb810e725144e06197590193e63ae8ca6fd95ad
gfhjftyfyif <93073478+gfhjftyfyif@users.noreply.github.com>
2021-11-26 23:02:52 +0800

    No5

---
+++ No5Level1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	auth := func(c *gin.Context){
+		value, err := c.Cookie("gin_cookie")
+		if err != nil {
+			c.JSON(403,gin.H{
+				"message":"认证失败,没有cookie",
+			})
+			c.Abort()
+		}else{
+			c.Set("cookie",value)
+			c.Next()
+			v,_:=c.Get("next")
+			fmt.Println(v)
+		}
+	}
+	r.POST("/login",func (c *gin.Context){
+		username:=c.PostForm("username")
+		password:=c.PostForm("password")
+		if username == "123" && password == "321"{
+			c.SetCookie("gin_cookie", username, 3600, "/", "", false, true)
+			c.JSON(200,gin.H{
+				"msg": "login successfully",
+			})
+		}else{
+			c.JSON(403,gin.H{
+				"message":"认证失败,账号密码错误",
+			})
+		}
+	})
+	r.GET("/hello",auth, func(c *gin.Context) {
+		cookie,_:=c.Get("cookie")
+		str:=cookie.(string)
+		c.String(200,"？？？"+str)
+		c.Set("next","test next")
+	})
+	r.Run()
+}
